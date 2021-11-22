f4c2e2f7d13a5c6521a6fe9937bd0e214680ecb0
michael lopez <mlopezcruz2015@fau.edu>
2021-11-22 18:13:51 -0500

    worked on algo.

--- Assignment3.py
+++ Assignment3.py
@@ -19,6 +19,7 @@ for n in exponents:
 x = dataframe[X_headers]
 y = dataframe[Y_header]
 
+x = x-x.mean()
 x = x/x.std()
 
 x.insert(loc=0, column='K', value=1)
@@ -27,7 +28,22 @@ y = y.map({'African':1, 'European':2, 'EastAsian':3, 'Oceanian':4, 'NativeAmeric
 y_mapped = y.map({1:np.array([1,0,0,0,0]), 2:np.array([0,1,0,0,0]), 3:np.array([0,0,1,0,0]), 4:np.array([0,0,0,1,0]), 5:np.array([0,0,0,0,1])})
 print(y_mapped)
 
-b = np.zeros((183, 5))
+b = np.zeros((11, 5))
+b = b.astype(float)
+
 y = np.zeros((183, 5))
+
+for l in range(10000):
+
+    u = np.exp(np.matmul(x, b))
+
+    p = np.zeros((u.shape[0], u.shape[1]))
+    for i in range(183):
+        for j in range(5):
+            p[i][j] = u[j][i]/(np.sum([u[0][i], u[0][i], u[1][i], u[2][i], u[3][i], u[4][i]]))
+
+    z = np.zeros((5, 11))
+
+    b = b + learningRateA*(np.subtract(np.multiply(np.transpose(x), (np.subtract(y,p))), np.multiply(2 * tuning_parameters_lambda[0], np.subtract(b, z))))
 
 print('done')
